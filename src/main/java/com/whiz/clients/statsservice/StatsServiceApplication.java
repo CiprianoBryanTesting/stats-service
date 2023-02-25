7034528b2e8feff34ceb7c71a2a7e368bfc51c43
Bryan Tony <ciprianot.bryan@gmail.com>
2023-02-25 12:14:40 -0500

    Enable conexion to Naming-Server

--- src/main/java/com/whiz/clients/statsservice/StatsServiceApplication.java
+++ src/main/java/com/whiz/clients/statsservice/StatsServiceApplication.java
@@ -2,8 +2,10 @@ package com.whiz.clients.statsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.*;
 import org.springframework.cloud.openfeign.*;
 
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class StatsServiceApplication {
--- src/main/java/com/whiz/clients/statsservice/proxy/ClientProxy.java
+++ src/main/java/com/whiz/clients/statsservice/proxy/ClientProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@FeignClient(name = "client-service", url = "localhost:8001")
+@FeignClient(name = "client-service")
 public interface ClientProxy {
     @GetMapping("/client")
     ResponseEntity<List<ClientDTO>> getAll();
