b8c125497f8c63fdf9e52a181610a9b87d80b2da
Bryan Tony <ciprianot.bryan@gmail.com>
2023-04-21 11:21:49 -0500

    CircuitBreaker added to Endpoint GetStudentsAgeAverage

--- src/main/java/com/scotia/student/stats/controller/StatsController.java
+++ src/main/java/com/scotia/student/stats/controller/StatsController.java
@@ -2,6 +2,8 @@ package com.scotia.student.stats.controller;
 
 import com.scotia.student.stats.controller.dto.*;
 import com.scotia.student.stats.service.*;
+import com.scotia.student.stats.util.enums.*;
+import io.github.resilience4j.circuitbreaker.annotation.*;
 import lombok.extern.slf4j.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
@@ -16,7 +18,13 @@ public class StatsController {
     private StatsService statsService;
 
     @GetMapping("/students")
+    @CircuitBreaker(name = "students-stats", fallbackMethod = "apiFallBackStudentsStats")
     public ResponseEntity<StatsDTO> getStudentsAgeAverage() {
         return ResponseEntity.ok(statsService.getStudentsAgeAverage());
+    }
+
+    public ResponseEntity<?> apiFallBackStudentsStats(Exception exception) {
+        log.info(StatsResponse.INTERNAL_SERVER_ERROR.getMessage() + ", Error={}", exception.getMessage());
+        return ResponseEntity.noContent().build();
     }
 }
